fa80d0d0f2e72f05d978b47d984f00ecd09ef792
John Lobi Tumbo <john.lobi.tumbo@gmail.com>
2024-07-25 12:24:31 +0300

    feat: user creation and listing

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from django.utils.decorators import classonlymethod
+from asgiref.sync import sync_to_async
+from .models import CustomUser
+from .serializer import UserSerializer
 
-# Create your views here.
+
+class UserListCreateView(View):
+    @classonlymethod
+    async def get(cls, request):
+        users = await sync_to_async(CustomUser.objects.all)()
+        serialized_data = UserSerializer().serialize(users)
+        return JsonResponse(serialized_data, safe=False)
+
+    @classonlymethod
+    async def post(cls, request):
+        data = request.POST
+        user = await sync_to_async(CustomUser.objects.create)(
+            username=data['username'],
+            email=data['email'],
+            password=data['password']
+        )
+        serialized_data = UserSerializer().serialize([user])
+        return JsonResponse(serialized_data, safe=False)
